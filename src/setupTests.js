2340d6589a49957b1a4a3dcbf7f4f533a3156b38
jeffng5 <jeffreykng@hotmail.com>
2024-01-19 16:47:33 -0500

    updated with tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,27 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { render } from '@testing-library/react'
+import App from "./App"
+import {Card} from "./Card"
+
+// smoke tests
+test('it renders without crashing', () => {
+    render(<App />);
+    });   
+  // snapshot tests
+it("matches App snapshot", function(){
+    const {asFragment} = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  })
+
+
+// smoke tests
+test('it renders without crashing', () => {
+    render(<Card />);
+    });   
+// snapshot tests
+it("matches Card snapshot", function(){
+    const {asFragment} = render(<Card />);
+    wexpect(asFragment()).toMatchSnapshot();
+  })
